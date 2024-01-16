fd13d7a662cfe9dc1fdb762fdcef9ad983e80157
Dana Mulyana <danabontot@gmail.com>
2024-01-16 15:53:13 +0700

    adding a key to the route

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,8 +8,8 @@ import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer(function ActivityForm() {
     const { activityStore } = useStore();
-    const {selectedActivity, createActivity, updateActivity, 
-        loading, loadActivity,loadingInitial} = activityStore;
+    const {createActivity, updateActivity, loading, 
+        loadActivity,loadingInitial} = activityStore;
     const { id } = useParams();
 
     const [activity, setActivity] = useState<Activity>({
