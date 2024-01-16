f5e861926b3070de988836e533a7516ff0141a6b
Dana Mulyana <danabontot@gmail.com>
2024-01-16 12:36:51 +0700

    MobX stric mode (FE)

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,13 +1,34 @@
 import { makeAutoObservable } from "mobx";
+import { Activity } from "../models/activity";
+import agent from "../api/agent";
 
 export default class ActivityStore {
-    title = 'Hello from mobx';
+    activities: Activity[] = [];
+    selectedActivity: Activity | null = null;
+    editMode = false;
+    loading = false;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this)
     }
 
-    setTitle = () => {
-        this.title = this.title + '!';
+    loadActivities = async () => {
+        this.setLoadingInitial(true);
+        try{
+            const activities = await agent.Activities.list();
+            activities.forEach(activity => {
+                activity.date = activity.date.split('T')[0];
+                this.activities.push(activity);
+            })
+        }catch(error){
+            console.error(error);
+        }finally{
+            this.setLoadingInitial(false);
+        }
+    }
+
+    setLoadingInitial = (state: boolean) => {
+        this.loadingInitial = state;
     }
 }
