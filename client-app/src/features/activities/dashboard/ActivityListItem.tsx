a0dc7d37da67a1d3e5acf8dba2b38b8a487b6b44
Dana Mulyana <danabontot@gmail.com>
2024-01-17 10:58:38 +0700

    End Section 9

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,23 +1,12 @@
 import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { Link } from "react-router-dom";
-import { SyntheticEvent, useState } from "react";
-import { useStore } from "../../../app/stores/store";
 
 interface Props {
     activity: Activity
 }
 
 export default function ActivityListItem({ activity } : Props) {
-    const [target, setTarget] = useState('');
-    const { activityStore } = useStore();
-    const { deleteActivity, loading} = activityStore;
-
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string){
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-    }
-    
     return (
         <Segment.Group>
             <Segment>
