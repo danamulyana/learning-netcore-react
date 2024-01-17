ebc9450598ed83a35c4794eafb36710afe7ace51
Dana Mulyana <danabontot@gmail.com>
2024-01-17 14:58:15 +0700

    added a not found component

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,9 +1,11 @@
-import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
 import ActivityDashbard from "../../features/activities/dashboard/ActivityDashbard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import TestErrors from "../../features/errors/TestError";
+import NotFound from "../../features/errors/NotFound";
 
 export const routes: RouteObject[] = [
     {
@@ -29,6 +31,18 @@ export const routes: RouteObject[] = [
             {
                 path: 'manage/:id',
                 element: <ActivityForm key='manage' />
+            },
+            {
+                path: 'errors',
+                element: <TestErrors />
+            },
+            {
+                path: 'not-found',
+                element: <NotFound />
+            },
+            {
+                path: '*',
+                element: <Navigate replace to='/not-found' />
             },
         ]
     }
