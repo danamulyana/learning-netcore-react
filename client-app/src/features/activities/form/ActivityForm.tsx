b0d51ddbf1cb4f1db2d54def591c2510607bc236
Dana Mulyana <danabontot@gmail.com>
2024-01-22 16:31:30 +0700

    End Section 15

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Activity } from "../../../app/models/activity";
+import { ActivityFormValues } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { v4 as uuid } from 'uuid'
 import { Formik, Form } from "formik";
@@ -21,15 +21,7 @@ export default observer(function ActivityForm() {
     const { id } = useParams();
     const navigate = useNavigate();
 
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: '',
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = yup.object({
         title: yup.string().required('the activity title is required'),
@@ -41,13 +33,16 @@ export default observer(function ActivityForm() {
     })
     
     useEffect(() => {
-        if(id) loadActivity(id).then(activity => setActivity(activity!))
+        if(id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)))
     }, [id,loadActivity]);
 
-    function handleFormSubmit(activity: Activity) {
+    function handleFormSubmit(activity: ActivityFormValues) {
         if(!activity.id){
-            activity.id = uuid();
-            createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+            const newActivity = {
+                ...activity,
+                id: uuid(),
+            }
+            createActivity(newActivity).then(() => navigate(`/activities/${newActivity.id}`));
         }else{
             updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
         }
