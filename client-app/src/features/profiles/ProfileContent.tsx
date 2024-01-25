f3c08049aeb23d792b58c13ddb9410997ff397fa
Dana Mulyana <danabontot@gmail.com>
2024-01-25 15:08:56 +0700

    End Section 18 | Challenge

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,6 +1,8 @@
 import { Tab, TabPane } from "semantic-ui-react";
 import ProfilePhoto from "./ProfilePhoto";
 import { observer } from "mobx-react-lite";
+import ProfileAbout from "./ProfileAbout";
+import { Profile } from "../../app/models/profile";
 
 interface Props {
     profile: Profile
@@ -8,7 +10,7 @@ interface Props {
 
 export default observer(function ProfileContent({profile} : Props){
     const panes = [
-        {menuItem: 'About', render: () => <TabPane>About Content</TabPane>},
+        {menuItem: 'About', render: () => <ProfileAbout />},
         {menuItem: 'Photos', render: () => <ProfilePhoto profile={profile} />},
         {menuItem: 'Events', render: () => <TabPane>Event Content</TabPane>},
         {menuItem: 'Followers', render: () => <TabPane>Followers Content</TabPane>},
